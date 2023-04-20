3c9ff013d38766080d91db7613be2d5cf57f079a
Mike Agbelou <mike.agbelou@gmail.com>
2023-04-20 22:11:22 +0200

    first commit to my blog projet

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'project-5',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'projet-5/blog',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'michelf/php-markdown' => array(
+            'pretty_version' => '1.9.1',
+            'version' => '1.9.1.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../michelf/php-markdown',
+            'aliases' => array(),
+            'reference' => '5024d623c1a057dcd2d076d25b7d270a1d0d55f3',
+            'dev_requirement' => false,
+        ),
+        'projet-5/blog' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'project-5',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
