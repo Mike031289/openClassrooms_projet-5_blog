75ab315af18903f72760fac55c4a0102beb1ddcc
Mike Agbelou <mike.agbelou@gmail.com>
2023-05-12 18:56:49 +0200

    creation and updating of new files in the app

--- index.php
+++ index.php
@@ -1,2 +1,30 @@
 <?php
-echo "salut mike";
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/app/views/templates');
+$twig = new Twig\Environment($loader);
+
+$router = new AltoRouter();
+
+$router->map('GET', '/', function() use ($twig) {
+    echo $twig->render('index.html.twig', [
+        'title' => 'Page d\'accueil',
+        'content' => 'Bienvenue sur mon blog !'
+    ]);
+});
+
+$router->map('GET', '/blog/[a:slug]', function($slug) use ($twig) {
+    echo $twig->render('blog.html.twig', [
+        'title' => 'Article',
+        'content' => 'Contenu de l\'article avec slug : ' . $slug
+    ]);
+});
+
+$match = $router->match();
+if($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
+    echo $twig->render('404.html.twig');
+}
