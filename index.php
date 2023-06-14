3fd3d274af4bbdd78e4eb97af768a37e6340ffd7
Mike Agbelou <mike.agbelou@gmail.com>
2023-06-14 22:51:52 +0200

    Reorganisation of file locations (architecture)

--- index.php
+++ index.php
@@ -22,7 +22,9 @@ spl_autoload_register(function($class) use($config)
 });
 try
 	{
+		echo "yes"; 
 		$httpRequest = new HttpRequest();
+		var_dump($httpRequest); die;
 		$router = new Router($config->baseUrl);
 		$httpRequest->setRoute($router->findRoute($httpRequest));
 	}
@@ -30,25 +32,3 @@ try
 	{
 		echo "Une erreur s'est produite : " . $e->getMessage();
 	}
-
-// $router->map('GET', '/', function() use ($twig) {
-//     echo $twig->render('index.html.twig', [
-//         'title' => 'Page d\'accueil',
-//         'content' => 'Bienvenue sur mon blog !'
-//     ]);
-// });
-
-// $router->map('GET', '/blog/[a:slug]', function($slug) use ($twig) {
-//     echo $twig->render('blog.html.twig', [
-//         'title' => 'Article',
-//         'content' => 'Contenu de l\'article avec slug : ' . $slug
-//     ]);
-// });
-
-// $match = $router->match();
-// if($match && is_callable($match['target'])) {
-//     call_user_func_array($match['target'], $match['params']);
-// } else {
-//     header($_SERVER['SERVER_PROTOCOL'] . ' 404 Not Found');
-//     echo $twig->render('404.html.twig');
-// }
