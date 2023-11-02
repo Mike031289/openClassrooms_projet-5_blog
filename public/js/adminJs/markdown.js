7c43b68c5b30a3b533b4478339b7c91b7f4086ef
Mike Agbelou <mike.agbelou@gmail.com>
2023-11-02 23:35:16 +0100

    display post list on dashbord

--- public/js/adminJs/markdown.js
+++ public/js/adminJs/markdown.js
@@ -1,4 +1,10 @@
-var easyMDE = new EasyMDE({
-    element: document.getElementById('postEditor'),
-    toolbar: ['bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', '|', 'preview', 'guide']
+// var easyMDE = new EasyMDE({
+//     element: document.getElementById('postEditor'),
+//     toolbar: ['bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', '|', 'preview', 'guide']
+// });
+document.addEventListener("DOMContentLoaded", function () {
+    var easyMDE = new EasyMDE({
+        element: document.getElementById('postEditor'),
+        toolbar: ['bold', 'italic', 'heading', '|', 'quote', 'unordered-list', 'ordered-list', '|', 'link', 'image', '|', 'preview', 'guide']
+    });
 });
