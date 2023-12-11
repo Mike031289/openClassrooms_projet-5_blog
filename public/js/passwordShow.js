33d661ad38254d16c9b2361600f3c912a1472d3e
Mike Agbelou <mike.agbelou@gmail.com>
2023-12-11 22:34:14 +0100

    login and register from relook

--- public/js/passwordShow.js
+++ public/js/passwordShow.js
@@ -1,33 +1,23 @@
+// Toggle password visibility
 $(document).ready(function () {
-
-  $("#show-password").click(function () {
-    const passwordField = $("#inputPassword");
-    const passwordIcon = $("#password-icon");
-    if (passwordField.attr("type") === "password") {
-      passwordField.attr("type", "text");
-      passwordIcon.removeClass("fa-eye");
-      passwordIcon.addClass("fa-eye-slash");
-    } else {
-      passwordField.attr("type", "password");
-      passwordIcon.removeClass("fa-eye-slash");
-      passwordIcon.addClass("fa-eye");
-    }
+  $('#showPasswordBtn').on('click', function () {
+    togglePasswordVisibility('#inputPassword', '#password-icon');
   });
 
-  $("#show-passWord").click(function () {
-    const passwordField1 = $("#inputPassWord");
-    const passwordConfirmField = $("#inputPassWordConfirm");
-    const passwordIcon = $("#passWord-icon");
-    if (passwordField1.attr("type") === "password" && passwordConfirmField.attr("type") === "password") {
-      passwordField1.attr("type", "text");
-      passwordConfirmField.attr("type", "text");
-      passwordIcon.removeClass("fa-eye");
-      passwordIcon.addClass("fa-eye-slash");
-    } else {
-      passwordField1.attr("type", "password");
-      passwordConfirmField.attr("type", "password");
-      passwordIcon.removeClass("fa-eye-slash");
-      passwordIcon.addClass("fa-eye");
-    }
+  $('#showConfirmPasswordBtn').on('click', function () {
+    togglePasswordVisibility('#inputConfirmPassword', '#confirm-password-icon');
   });
+
+  function togglePasswordVisibility(inputId, iconId) {
+    var passwordInput = $(inputId);
+    var passwordIcon = $(iconId);
+
+    if (passwordInput.attr('type') === 'password') {
+      passwordInput.attr('type', 'text');
+      passwordIcon.removeClass('fa-eye').addClass('fa-eye-slash');
+    } else {
+      passwordInput.attr('type', 'password');
+      passwordIcon.removeClass('fa-eye-slash').addClass('fa-eye');
+    }
+  }
 });
