efd99e491d9d6e5a753d305259c25edb0352fb38
Mike Agbelou <mike.agbelou@gmail.com>
2023-12-28 19:52:33 +0100

    rewrite php hints for UserManager class in Manager/UserManager.php

--- app/Manager/UserManager.php
+++ app/Manager/UserManager.php
@@ -7,8 +7,18 @@ namespace App\Manager;
 use App\Exceptions\ActionNotFoundException;
 use App\Models\User;
 
+/**
+ * Class UserManager
+ *
+ * Manages user-related operations.
+ */
 class UserManager extends BaseManager
 {
+    /**
+     * UserManager constructor.
+     *
+     * @param object $dataSource The data source object.
+     */
     public function __construct(object $dataSource)
     {
         parent::__construct('user', 'User', $dataSource);
@@ -16,6 +26,10 @@ class UserManager extends BaseManager
 
     /**
      * Get a user by their email from the database.
+     *
+     * @param string $email The email of the user.
+     *
+     * @return User|null The User object if found, or null otherwise.
      */
     public function getUserByEmail(string $email): ?User
     {
@@ -37,12 +51,16 @@ class UserManager extends BaseManager
         // Use fetchObject to retrieve the result as an object of the User class
         $user = $stmt->fetchObject(User::class);
 
-        // Return the User object or false if not found
-        return $user ? $user : null;
+        // Return the User object or null if not found
+        return $user ?: null;
     }
 
     /**
      * Get a user by their name from the database.
+     *
+     * @param string $userName The username of the user.
+     *
+     * @return User|null The User object if found, or null otherwise.
      */
     public function getUserByName(string $userName): ?User
     {
@@ -64,18 +82,18 @@ class UserManager extends BaseManager
         // Use fetchObject to retrieve the result as an object of the User class
         $user = $stmt->fetchObject(User::class);
 
-        // Return the User object or false if not found
-        return $user ? $user : null;
+        // Return the User object or null if not found
+        return $user ?: null;
     }
 
     /**
      * Create a new user in the database.
      *
-     * @param $userName The username of the new user
-     * @param $email    The email address of the new user
-     * @param $passWord The password of the new user (plain text)
+     * @param string $userName The username of the new user.
+     * @param string $email The email address of the new user.
+     * @param string $passWord The password of the new user (plain text).
      *
-     * @return User|null returns a User object if the user was successfully created, or null on failure
+     * @return User|null The User object if the user was successfully created, or null on failure.
      */
     public function createUserWithRole(string $userName, string $email, string $passWord): ?User
     {
@@ -85,19 +103,13 @@ class UserManager extends BaseManager
         // Use a transaction to ensure data consistency
         $this->_db->beginTransaction();
 
-        // Get the current date
-        $date = new \DateTime();
-        $date->setTimezone(new \DateTimeZone('Europe/Paris')); // Set the timezone if necessary
-        $createdAt = $date->format('Y-m-d H:i:s');
-
         try {
             // Step 1: Insert the user into the 'user' table
-            $sql = 'INSERT INTO user (userName, email, passWord, createdAt) VALUES (?, ?, ?, ?)';
+            $sql  = 'INSERT INTO user (userName, email, passWord, createdAt) VALUES (?, ?, ?, NOW())';
             $stmt = $this->_db->prepare($sql);
             $stmt->bindParam(1, $userName, \PDO::PARAM_STR);
             $stmt->bindParam(2, $email, \PDO::PARAM_STR);
             $stmt->bindParam(3, $hashedPassword, \PDO::PARAM_STR);
-            $stmt->bindParam(4, $createdAt, \PDO::PARAM_STR);
 
             if (!$stmt->execute()) {
                 throw new ActionNotFoundException();
@@ -108,15 +120,13 @@ class UserManager extends BaseManager
 
             // Step 3: Retrieve the roleId from the 'role' table (adjust the SQL query as needed)
             $roleName = 'Visitor'; // Replace with the actual role name
-            $sql = 'SELECT roleId FROM role WHERE roleName = ?';
-            $stmt = $this->_db->prepare($sql);
+            $sql      = 'SELECT roleId FROM role WHERE roleName = ?';
+            $stmt     = $this->_db->prepare($sql);
             $stmt->bindParam(1, $roleName, \PDO::PARAM_STR);
 
             if ($stmt->execute()) {
-                $roleRow = $stmt->fetch(\PDO::FETCH_ASSOC);
-                if ($roleRow) {
-                    $roleId = $roleRow['roleId'];
-                } else {
+                $roleId = $stmt->fetchColumn();
+                if (!$roleId) {
                     throw new ActionNotFoundException();
                 }
             } else {
@@ -124,7 +134,7 @@ class UserManager extends BaseManager
             }
 
             // Step 4: Insert the user ID and role into the 'userrole' table
-            $sql = 'INSERT INTO userrole (userId, roleId) VALUES (?, ?)';
+            $sql  = 'INSERT INTO userrole (userId, roleId) VALUES (?, ?)';
             $stmt = $this->_db->prepare($sql);
             $stmt->bindParam(1, $id, \PDO::PARAM_INT);
             $stmt->bindParam(2, $roleId, \PDO::PARAM_INT);
@@ -142,13 +152,12 @@ class UserManager extends BaseManager
             $user->setUserName(htmlspecialchars($userName));
             $user->setEmail(htmlspecialchars($email));
             $user->setPassWord(htmlspecialchars($hashedPassword));
-            $user->setCreatedAt(new \DateTime($createdAt));
+            $user->setCreatedAt(new \DateTime());
 
             // Return the User object
             return $user;
-        } catch (ActionNotFoundException $e) {
-            // Redirect to a 404 error page if no matching route is found
-            header('Location: 500');
+        }
+        catch (ActionNotFoundException $e) {
             // Handle the error in case of failure and roll back the transaction
             $this->_db->rollBack();
 
@@ -159,46 +168,51 @@ class UserManager extends BaseManager
     /**
      * Retrieve the role of a user by their email.
      *
-     * @param              $email The email of the user
-     * @return string|null the user's role or null if not found
+     * @param string $email The email of the user.
+     *
+     * @return string|null The user's role or null if not found.
      */
     public function getUserRoleByEmail(string $email): ?string
     {
         $sql = 'SELECT r.roleName
-            FROM user AS u
-            JOIN userrole AS ur ON u.id = ur.userId
-            JOIN role AS r ON ur.roleId = r.roleId
-            WHERE u.email = :email';
+        FROM user AS u
+        JOIN userrole AS ur ON u.id = ur.userId
+        JOIN role AS r ON ur.roleId = r.roleId
+        WHERE u.email = :email';
 
-        $req = $this->_db->prepare($sql);
-        $req->bindValue(':email', $email, \PDO::PARAM_STR);
-        $req->execute();
+        $stmt = $this->_db->prepare($sql);
+        $stmt->bindValue(':email', $email, \PDO::PARAM_STR);
+        $stmt->execute();
 
-        $result = $req->fetch(\PDO::FETCH_ASSOC);
+        // Utiliser fetchColumn pour obtenir directement la valeur de 'roleName'
+        $roleName = $stmt->fetchColumn();
 
-        return ($result) ? $result['roleName'] : null;
+        return ($roleName !== false) ? (string) $roleName : null;
     }
 
     /**
      * Retrieve the role of a user by their id.
      *
-     * @param              $id The id of the user
-     * @return string|null the user's role or null if not found
+     * @param int $id The ID of the user.
+     *
+     * @return string|null The user's role or null if not found.
      */
     public function getAuthorRoleById(int $id): ?string
     {
         $sql = 'SELECT r.roleName
-            FROM user AS u
-            JOIN userrole AS ur ON u.id = ur.userId
-            JOIN role AS r ON ur.roleId = r.roleId
-            WHERE u.id = :id';
+        FROM user AS u
+        JOIN userrole AS ur ON u.id = ur.userId
+        JOIN role AS r ON ur.roleId = r.roleId
+        WHERE u.id = :id';
 
-        $req = $this->_db->prepare($sql);
-        $req->bindValue(':id', $id, \PDO::PARAM_STR);
-        $req->execute();
+        $stmt = $this->_db->prepare($sql);
+        $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+        $stmt->execute();
 
-        $result = $req->fetch(\PDO::FETCH_ASSOC);
+        // Utiliser fetchColumn pour obtenir directement la valeur de 'roleName'
+        $roleName = $stmt->fetchColumn();
 
-        return ($result) ? $result['roleName'] : null;
+        return ($roleName !== false) ? (string) $roleName : null;
     }
+
 }
